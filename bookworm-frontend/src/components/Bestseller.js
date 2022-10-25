f2be77803d96686a3a46b4642dd02fa113b3d381
Hafizuddin <lapizjusoh@yahoo.com>
2022-10-25 13:07:29 +0800

    Fix missing bootstrap

--- bookworm-frontend/src/components/Bestseller.js
+++ bookworm-frontend/src/components/Bestseller.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 class Bestseller extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      baseURL: 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json',
+      apiKey: '?api-key=Bnbbb1JMRcU4yMb1rxevxLLGPEsyJOjA',
+      listingURL: '',
+      bestsellerList: ''
+    }
+    this.showBestsellers = this.showBestsellers.bind(this);
+  }
+
+  showBestsellers(){
+    this.setState({
+      listingURL: this.state.baseURL + this.state.apiKey
+    },()=>{
+      fetch(this.state.listingURL)
+      .then(res=>{
+        return res.json();
+      })
+      .then(json=> this.setState({ bestsellerList: json }))
+    })
+  }
+
   render(){
     return (
-      <div>
-        <h1>This is the Bestseller</h1>
+      <div onLoad={this.showBestsellers}>
+        <h1>{this.state.bestsellerList.copyright}</h1>
       </div>
     )
   }
